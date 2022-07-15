eb7f0c64ee96380e33983138a38a5e332257016f
Ian <biboyboy04@gmail.com>
2022-07-15 14:41:10 +0800

    Remove spaces in index.html and put comments in game.js

--- game.js
+++ game.js
@@ -11,10 +11,11 @@ function titleCase(str){
 }
 
 function playRound(playerSelection, computerSelection){
+    // if playerSelection is not truthy, return the string and don't run the code below
     if(!playerSelection){
         return "Null or Undefined"
     }
-
+    // Checks whether playerSelection selected a correct choice
     if ((titleCase(playerSelection) == "Rock") || (titleCase(playerSelection) == "Scissors") || (titleCase(playerSelection) == "Paper")){
 
         if((titleCase(playerSelection) == "Rock" && computerSelection == "Scissors") || (titleCase(playerSelection) == "Scissors") && (computerSelection == "Paper") || (titleCase(playerSelection) == "Paper") && (computerSelection == "Rock")){
@@ -32,9 +33,10 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-// starts the game
+// call playRound function and prompt 5 times
 function game(){
     for (let i = 0; i < 5; i++) {
-        // your code here!
+        let userInput = prompt("Rock, Paper, or Scissors? ", "")
+        console.log(playRound(userInput, computerPlay()))
      }
 }
