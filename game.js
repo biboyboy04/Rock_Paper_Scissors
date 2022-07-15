cbe7d1192fc5faedb2664494fc0e82ab933e9898
Ian <biboyboy04@gmail.com>
2022-07-15 13:02:02 +0800

    Add index.html and game.js
    Add a blank html document linked to a javascript file. The game.js have two blank functions and one computerPlay function that returns a random move; It is accomplished by
    creating an array with the moves as its items and accesing the index of the array with an random number generator.

---
+++ game.js
@@ -0,0 +1,16 @@
+// returns random computer moves
+function computerPlay(){
+    const moves = ["Rock", "Paper", "Scissors"];
+    // returning a random index of moves array from 0-2
+    return moves[Math.floor(Math.random() * 3)];
+}
+
+function playRound(playerSelection, computerSelection){
+    
+}
+
+// starts the game
+function game(){
+
+
+}
