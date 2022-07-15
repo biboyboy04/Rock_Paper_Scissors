c182c95b8a83ffff9dcab8487d6986738c8d423b
Ian <biboyboy04@gmail.com>
2022-07-15 14:16:47 +0800

    Add titleCase function and playRound function
    titleCase is for making the user input case-insensitive. The playRound function accepts two parameters and is used to compare the result of the round. Furthermore, the playRound function checks whether the user input is not a truthy value; If it is, it returns a string. Otherwise, it proceeds to the code below.

--- game.js
+++ game.js
@@ -5,12 +5,36 @@ function computerPlay(){
     return moves[Math.floor(Math.random() * 3)];
 }
 
+function titleCase(str){
+    // returns the capitalize first letter + the rest of the string
+    return str[0].toUpperCase() + str.substr(1).toLowerCase() 
+}
+
 function playRound(playerSelection, computerSelection){
-    
+    if(!playerSelection){
+        return "Null or Undefined"
+    }
+
+    if ((titleCase(playerSelection) == "Rock") || (titleCase(playerSelection) == "Scissors") || (titleCase(playerSelection) == "Paper")){
+
+        if((titleCase(playerSelection) == "Rock" && computerSelection == "Scissors") || (titleCase(playerSelection) == "Scissors") && (computerSelection == "Paper") || (titleCase(playerSelection) == "Paper") && (computerSelection == "Rock")){
+            return `You WIN! ${playerSelection} beats ${computerSelection}.`
+        }
+        else if((titleCase(playerSelection) == "Rock") && (computerSelection == "Paper") || (titleCase(playerSelection) == "Scissors") && (computerSelection == "Rock") || (titleCase(playerSelection) == "Paper") && (computerSelection == "Scissors")){
+            return`You LOSE! ${computerSelection} beats ${playerSelection}.`
+        }
+        else{
+            return `It's a TIE! ${playerSelection} is even with ${computerSelection}.`
+        }
+    }
+    else {
+        return "Invalid input."
+    }
 }
 
 // starts the game
 function game(){
-
-
+    for (let i = 0; i < 5; i++) {
+        // your code here!
+     }
 }
